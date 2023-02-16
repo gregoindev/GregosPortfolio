809a2d825076648d1b09940ef94ea913590dff5d
gregoindev <gregocodes23@gmail.com>
2023-02-15 21:24:49 -0800

    Initial commit

---
+++ app/src/main/java/com/gregorilla/gregosportfolio/MainActivity.java
@@ -0,0 +1,31 @@
+package com.gregorilla.gregosportfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView list = findViewById(R.id.recycler_view_projects);
+
+
+        Project[] projects = {
+                new Project("Getting Started App", "Our very first project, the default 'Hello World' app!", R.drawable.getting_started),
+                new Project("MyQuote", "A single quote app!", R.drawable.quote),
+                new Project("DollarToPeso ", "An app that takes a Dollar Currency and converts it to Peso, teaching me Variables, Methods and Conditional Logic", R.drawable.calculator),
+                new Project("Converter", "Converter app!", R.drawable.tape),
+                new Project("Kainan", "A menu app for a restaurant called 'Kainan', learning about Activities, Classes & Objects, Arrays, Intents and ListViews", R.drawable.hungry_developer),
+
+        };
+
+        ProjectsAdapter adapter = new ProjectsAdapter(projects);
+
+        list.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/gregorilla/gregosportfolio/Project.java
@@ -0,0 +1,21 @@
+package com.gregorilla.gregosportfolio;
+
+import android.widget.ImageView;
+
+public class Project {
+    String name, description;
+    int image;
+
+
+    public Project(String name, String description, int image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+
+
+
+
+
+}
